589f16671942ee975cb340c92696c282363d63c6
Shantanu-61902 <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-04 16:49:36 +0000

    Datatypes and ECMA Standards

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,17 @@
+"use strict" //treat all js code as new version
+// alert(3+3) we are using nodejs not browser
+console.log(3+3);
+console.log("Shantanu Mishra")
+/*
+number => 2^53
+bigint
+string =>""
+boolen=>true/false
+null => standalone
+undefined => undefined 
+symbol => unique
+object */
+console.log(typeof "Shantanu Mishra")
+console.log(typeof null)
+console.log(typeof object)
+
