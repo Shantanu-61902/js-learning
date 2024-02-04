059cf643cf5da68b7561c52f800ce88b0974745e
Shantanu-61902 <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-04 16:13:49 +0000

    Hi

---
+++ 01basics/01variable.js
@@ -0,0 +1,11 @@
+const accountId = 234
+let accountEmail = "shanu@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+accountEmail="hc@hc.com"
+accountPassword="123"
+accountCity="Bengaluru"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+// accountId = 2
+console.log(accountId);
