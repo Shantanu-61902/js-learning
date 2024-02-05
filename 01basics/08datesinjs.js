1842856afd26d43d9ba67d3f5f03a1ca2a5a6901
Shantanu-61902 <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-05 15:28:10 +0000

    DATE&TIME

---
+++ 01basics/08datesinjs.js
@@ -0,0 +1,29 @@
+//Dates
+const myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// const newDate = new Date(2023,0,23)
+// console.log(newDate.toDateString());
+// const nnewDate = new Date(2024,11,2,17,30)
+// const nnewDate = new Date("2024-12-02")
+const nnewDate = new Date("01-14-2021")
+// console.log(nnewDate.toLocaleString());
+
+const myTimestamp = Date.now()
+// console.log(myTimestamp);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+console.log(newDate.getMinutes());
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
