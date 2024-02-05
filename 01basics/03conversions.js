e837a1a81a85c1af90955a5d9f2b18463d7ee646
Shantanu-61902 <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-05 07:09:52 +0000

    Conversions&Operations

--- 01basics/03conversions.js
+++ 01basics/03conversions.js
@@ -3,18 +3,54 @@ console.log(typeof score);
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
-/*
-"33" = 33
-"33abc" = NaN
-true => 1 
-false => 0
-*/
-let isLoggedIn = ""
-let booleanisLoggedIn = Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);
+ /*
+ "33" = 33
+ "33abc" = NaN
+ true => 1 
+ false => 0
+ */
+ let isLoggedIn = ""
+ let booleanisLoggedIn = Boolean(isLoggedIn);
+ console.log(booleanisLoggedIn);
 /*
 0=>fasle 
 1=>true
 ""=>false
 "shanu or any value"=> true
 */
+ let someNumber = 65
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//******OPERATIONS****** */
+ let value = 69
+ let negvalue = -value;
+ console.log(negvalue);
+
+ console.log(6+6);
+ console.log(6-6);
+ console.log(6*6);
+ console.log(2**3);
+ console.log(6/6);
+ console.log(6%6);
+
+ let str1 = "Hey!"
+ let str2 = " Shantanu Mishra"
+ let str3 = str1+str2;
+ console.log(str3);
+
+ console.log("1"+2);
+ console.log(1+"2");
+ console.log("1"+2+2);
+ console.log(1+2+"2");
+
+ console.log(+true);
+ console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=6+9
+console.log(num3);
+let gameCounter = 1000;
+gameCounter++;
+console.log(gameCounter);
