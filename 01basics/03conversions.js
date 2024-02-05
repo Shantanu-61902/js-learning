19119e124aede80983505f24e5c4f32e7e409cb0
Shantanu-61902 <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-05 06:37:05 +0000

    03Conversions

--- 01basics/03conversions.js
+++ 01basics/03conversions.js
@@ -3,3 +3,9 @@ console.log(typeof score);
 let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+/*
+"33" = 33
+"33abc" = NaN
+true => 1 
+false => 0
+*/
