d5cafac31a6e4ee61ce859ee2c70c65121112e78
Shantanu-61902 <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-05 07:58:08 +0000

    DatatypesSummary

---
+++ 01basics/05datatypesummary.js
@@ -0,0 +1,32 @@
+//PRIMITIVE DATATYPES
+// 7 types - String,Number,Boolean,Symbol,Undefined,Null,BigInt
+const score = 100
+const scoreValue = 100.67
+
+const isLoggedIn = true
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+console.log(userEmail);
+console.log(outsideTemp);
+console.log(scoreValue);
+
+const bigNumber = 32589357293872n;
+console.log(typeof bigNumber);
+
+// Reference(Non-Primitive DataTypes)-Arrays,Functions,Objects
+const avengers = ["Ironman","Hawkeye","Steve Rogers"];
+let myobj = {
+    name: "Shantanu Mishra",
+    age: 22,
+    DOB:250611
+}
+const myFunction = function(){
+    console.log("Hello Shantanu Mishra");
+}
+console.log(myFunction);
+console.log(avengers);
+console.log(myobj);
