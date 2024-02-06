16cc535a735a2a2b2f50ac0e523aa1d368238171
Shantanu-61902 <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-06 15:58:56 +0000

    JSON

---
+++ 02basics/05objectsdestructingjsonapi.js
@@ -0,0 +1,13 @@
+const course = {
+    courseName : "JS By Chai Aur Code",
+    coursePrice: 499,
+    courseInstructor: "Shrelock"
+}
+
+const {coursePrice:a,courseInstructor:b,courseName:c} = course  //In this we can also give name accordingingly to small names or a single variable for e.g. coursePrice->a , courseInstructor->b and so on.....
+console.log(a);
+console.log(b);
+console.log(c);
+
+// 
+//JSON is a string format
