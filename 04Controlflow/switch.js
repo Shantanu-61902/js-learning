5b541ca81f196c5655c080c2711cfa7b6e829347
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-07 12:44:58 +0000

    Switch

---
+++ 04Controlflow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+const month = "march"
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "February":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default Value");
+        break;
+}
