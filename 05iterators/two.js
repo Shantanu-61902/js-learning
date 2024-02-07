b8081cf741ca51eb64b242f7949ea90b07726beb
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-02-07 16:59:04 +0000

    while and do while loops

---
+++ 05iterators/two.js
@@ -0,0 +1,19 @@
+// while and do while
+
+let index = 0
+while(index <=10){
+    console.log(`The Value of Index is = ${index}`);
+    index = index + 3;
+}
+ let myArray = ["Starlord","Gamora","Rocket","Nebula"]
+ let arr = 0
+ while(arr < myArray.length){
+    console.log(`The Value is ${myArray[arr]} `);
+    arr = arr + 1
+ }
+
+ let score = 10
+ do {
+    console.log(`The value of score = ${score}`);
+    score = score + 1
+ } while (score <= 15);
