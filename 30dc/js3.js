08c0362406ee42648497ff9190137dd5acf9860c
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-03-20 18:00:24 +0000

    30dc-03

---
+++ 30dc/js3.js
@@ -0,0 +1,12 @@
+switch(ch){
+         case 1 :
+            console.log("I am one..");
+            break;
+         case 2 :
+             console.log("I am two..");
+         case 3 :
+             console.log("I am three..");
+         default:
+             console.log("You pressed wrong choice!");
+             break;
+     }
