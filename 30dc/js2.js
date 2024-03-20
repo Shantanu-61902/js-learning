a6d8b37e217fa766613f16abeb3d12d5bfcddcd4
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-03-20 17:59:34 +0000

    30dc-2

---
+++ 30dc/js2.js
@@ -0,0 +1,13 @@
+let a = 10;
+ let b = 15;
+
+ if(a>b){
+    console.log("A is Greater");
+ }
+ else if(b>a)
+ {
+     console.log("B is greater");
+ }
+ else{
+     console.log("Both are equal");
+ }
