1eda7a366ef40fbba6ef2f144cc531c7e06b849d
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-03-20 18:01:15 +0000

    30dc-04

---
+++ 30dc/js4.js
@@ -0,0 +1,20 @@
+//      ***LOOPS***
+
+ for(var i = 0;i<=11;i++){
+     //console.log("2 x " +i+ " = "+ 2*i);
+     console.log(`2 X ${i} = ${2*i}`);
+ } 
+
+ //while loop
+
+ var i =1
+ while(i<=10){
+     console.log(`3 x ${i} = ${3*i}`);
+     i++;
+ } 
+// //do while
+ var i = 1;
+ do{
+     console.log(`5 x ${i} = ${5*i}`);
+     i++;
+ }while(i<11)
