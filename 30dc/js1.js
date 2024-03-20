1a10dd672bef1d6b310fcf980f8fb75a9ef1de66
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-03-20 17:58:05 +0000

    30dc

---
+++ 30dc/js1.js
@@ -0,0 +1,19 @@
+// this is comment
+
+/* Multi line comment in js*/
+console.log("Hello Shantanu");
+let age = 18;
+ let name = "JohnCena"
+
+ if(age >= 18){
+     if(name == "JohnCena"){
+     console.log("You are Eligible!");
+ }
+
+ else{
+     console.log("Name is not there!");
+ }
+ }
+ else {
+     console.log("Not eligible");
+ }
