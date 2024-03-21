d27d824bdf7433f63758f68c28f01947a9f80a66
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-03-21 18:12:29 +0000

    30dc js05

---
+++ 30dc/js5.js
@@ -0,0 +1,13 @@
+//Different variacles
+ "use strict"
+ let a = 5       //
+ const b = 7
+ var c = 9
+ let _myname = "Save your Tears";
+ console.log(_myname);
+ console.log(a);
+ console.log(b);
+ console.log(c);
+ datatypes
+ let num = 50;
+ console.log(typeof(num));
