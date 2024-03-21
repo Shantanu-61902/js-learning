8775790a3ecd7613cfabb52f69891ee66e4b1c99
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-03-21 18:13:12 +0000

    30dc js6

---
+++ 30dc/js6.js
@@ -0,0 +1,11 @@
+ let str = "AB De Villiers"
+console.log(typeof(str));
+
+ let bool = false;
+ console.log(typeof(bool));
+ let a = null 
+ console.log(typeof(a));
+
+ let b;
+ console.log(b);
+ console.log(typeof(b));
