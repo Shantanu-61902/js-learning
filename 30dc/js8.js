29a82a7523fb83a645eeacfc2a92dd8cd4d24d12
Shantanu Mishra <119602419+Shantanu-61902@users.noreply.github.com>
2024-03-21 18:14:40 +0000

    30dc js8

---
+++ 30dc/js8.js
@@ -0,0 +1,25 @@
+//Operators
+//arithmetic operators
+ console.log(2+5);
+ console.log(2-5);
+ console.log(2*5);
+ console.log(50/10);
+console.log(50%10);
+ console.log(10**2);
+
+  let a = 1;
+ console.log(a++);
+ console.log(a);
+console.log(++a);
+
+  a = true;
+  b = 1;
+  console.log(a === b);
+  console.log(a != b);
+  console.log(a <= b);
+  console.log(a >= b);
+ console.log(a != b); 
+
+console.log(4>3 || 3>1 );
+console.log(4>3 && 1>1 );
+console.log(!false);
