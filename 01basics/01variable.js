d95efa85a686eca5de8f2e324a6a65f8ba6c628c
Shantanu-61902 <shantanu831822@gmail.com>
2025-02-26 00:25:47 +0530

    Added a Txt File

--- 01basics/01variable.js
+++ 01basics/01variable.js
@@ -3,7 +3,7 @@ let accountEmail = "shanu@gmail.com"
 var accountPassword = "1234"
 accountCity = "Jaipur"
 
-accountEmail="hc@hc.com"
+accountEmail="hc@hc.comm"
 accountPassword="123"
 accountCity="Bengaluru"
 let accountState
